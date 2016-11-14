b4e0b9d9a757485904f9c98761675e1f2e84c758
Ariadna Eyzaguirre <ariadna.eyzaguirre@gmail.com>
2016-11-14 12:31:47 -0500

    mensaje de register

--- js/register.js
+++ js/register.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-    
+    $("form").validate({
+        rules: {
+            phone: "required",  
+        },
+        messages: {
+            phone: "Please enter your phone",
+        },
+    });
 });
