85b082947f5f34f321cf7483938109feab572f20
Ariadna Eyzaguirre <ariadna.eyzaguirre@gmail.com>
2016-11-15 03:53:06 -0500

    number random

--- js/register.js
+++ js/register.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    $('form').submit(function() {
-        $('#btn-next').attr('disabled');
+    $("#section-sign-up").submit(function() {
+        $("#btn-next").attr('disabled');
     });
-    $('form').validate({
+    $("#section-sign-up").validate({
         rules: {
             phone:{
                 required: true,
@@ -17,10 +17,51 @@ $(document).ready(function () {
             }
         },
         submitHandler: function(form){
-            $('#btn-next').removeAttr('disabled');
+            $("#btn-next").removeAttr("disabled");
         }
     });
-/*    $('#btn-next').click(function(){
-        
+    
+    $("#btn-next").click(function(){
+        var numRandom = Math.floor((Math.random() * 9000) + 1000);
+        alert("This is your code" + numRandom);
+    });
+    
+    /*$("#section-date").validate({
+        rules: {
+            firstName:{
+                required: true,
+                number: false,                
+                minlength: 2
+            },
+            lastName:{
+                required: true,
+                number: false,                
+                minlength: 2
+            },
+            email:{
+                required: true,
+                email: true,
+                minlength: 2
+            }
+        },
+        messages: {
+            firstName: {
+                required: "Please enter your name",
+                number:"Please enter only letters"
+            },
+            lastName: {
+                required: "Please enter your last name",
+                number: "Please enter only letters"
+            },
+            email: {
+                required: "Please enter your email",
+                email: "Please enter your email with @"
+            }
+            
+        },
+        submitHandler: function(form){
+            $("#btn-next").removeAttr("disabled");
+        }
     });*/
+    
 });
